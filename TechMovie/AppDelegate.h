4583f8f585e9cbdf8505a80544903b32064e9b07
weed <weed_7777@yahoo.co.jp>
2012-07-11 05:42:33 +0900

    Initial Commit

---
+++ TechMovie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TechMovie
+//
+//  Created by 達郎 植田 on 12/07/11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TechMovie/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TechMovie
+//
+//  Created by 達郎 植田 on 12/07/11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
